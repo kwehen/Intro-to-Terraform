6658b17b5a360f83a37d83a293a776325fc87dfc
kwehen <110314567+kwehen@users.noreply.github.com>
2023-06-08 01:57:19 +0000

    Update project-vpc.tf
    Added private subnet Network ACL

--- project-vpc.tf
+++ project-vpc.tf
@@ -214,7 +214,61 @@ tags = {
 }
 }
 
+resource "aws_network_acl" "tf-private-acl" {
+  vpc_id = aws_vpc.tf-project-vpc.id
+
+    ingress {
+    protocol = "tcp"
+    rule_no = 100
+    action = "allow"
+    cidr_block = "10.1.0.0/16"
+    from_port = 22
+    to_port = 22
+  }
+    ingress {
+    protocol = "tcp"
+    rule_no = 200
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 80
+    to_port = 80
+  }
+    ingress {
+    protocol = "udp"
+    rule_no = 300
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 53
+    to_port = 53
+  }
+    ingress {
+    protocol = "tcp"
+    rule_no = 400
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 443
+    to_port = 443
+  }
+      egress {
+      protocol = -1
+      rule_no = 100
+      action = "allow"
+      cidr_block = "0.0.0.0/0"
+      from_port = 0
+      to_port = 0
+    }
+
+    tags = {
+      Name = "tf-project-private-acl"
+    }
+}
+
 resource "aws_network_acl_association" "tf-public-acl-assoc" {
   network_acl_id = aws_network_acl.tf-public-acl.id
   subnet_id = aws_subnet.tf-project-public.id
+}
+
+resource "aws_network_acl_association" "tf-private-acl-assoc" {
+  network_acl_id = aws_network_acl.tf-private-acl.id
+  subnet_id = aws_subnet.tfr-project-private.id
 }
