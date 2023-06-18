1c5e0bb6059c3a4b469254117292c32f3d0cbcb0
kwehen <110314567+kwehen@users.noreply.github.com>
2023-06-18 20:37:20 +0000

    Update s3-config.tf
    Secure s3 configuration after running tfsec

--- s3-config.tf
+++ s3-config.tf
@@ -7,8 +7,16 @@ resource "aws_s3_bucket" "<UNIQUE-BUCKET-NAME>" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "<UNIQUE-BUCKET-NAME-BLOCK>" {
+  bucket = aws_s3_bucket.<UNIQUE-BUCKET-NAME>.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_ownership_controls" "tf-project-own-control" {
- bucket = aws_s3_bucket.t<UNIQUE-BUCKET-NAME>.id
+  bucket = aws_s3_bucket.<UNIQUE-BUCKET-NAME>.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
@@ -21,6 +29,7 @@ resource "aws_s3_bucket_acl" "tf-project-s3-acl" {
   acl    = "private"
 }
 
+#tfsec:ignore:aws-s3-enable-bucket-logging
 resource "aws_s3_bucket" "<UNIQUE-LOG-BUCKET-NAME>" {
   bucket        = "<UNIQUE-LOG-BUCKET-NAME>"
   force_destroy = true
@@ -29,14 +38,29 @@ resource "aws_s3_bucket" "<UNIQUE-LOG-BUCKET-NAME>" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "tf-project-log-public-block" {
+  bucket = aws_s3_bucket.<UNIQUE-LOG-BUCKET-NAME>.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_logging" "tf-project-logging" {
   bucket = aws_s3_bucket.<UNIQUE-BUCKET-NAME>.id
 
-  target_bucket = aws_s3_bucket.<UNIQUE-LOG-BUCKET-NAME>.id
+  target_bucket = aws_s3_bucket.<UNIQUE-LOG-BUCKET-NAME>id
   target_prefix = "logs/"
 }
 
 resource "aws_s3_bucket_versioning" "tf-bucket-versioning" {
+  bucket = aws_s3_bucket.<UNIQUE-BUCKET-NAME>.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf-log-bucket-versioning" {
   bucket = aws_s3_bucket.<UNIQUE-BUCKET-NAME>.id
   versioning_configuration {
     status = "Enabled"
@@ -62,7 +86,9 @@ resource "aws_s3_bucket_policy" "tf-project-bucket-policy" {
             "s3:PutObject",
             "s3:PutObjectAcl",
             "s3:GetObject",
-            "s3:GetObjectAcl"
+            "s3:GetObjectAcl",
+            "s3:ListBucket",
+            "s3:GetBucketLocation"
         ],
       "Resource": [
         "${aws_s3_bucket.<UNIQUE-BUCKET-NAME>.arn}",
@@ -77,10 +103,23 @@ EOF
 resource "aws_kms_key" "tf-project-bucket-key" {
   description             = "Encryption key for Project Bucket"
   deletion_window_in_days = 10
+  enable_key_rotation = true
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "tf-project-sse" {
   bucket = aws_s3_bucket.<UNIQUE-BUCKET-NAME>.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.<UNIQUE-BUCKET-NAME>.arn
+      sse_algorithm     = "aws:kms"
+    }
+    bucket_key_enabled = true
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf-project-log-sse" {
+  bucket = aws_s3_bucket.<UNIQUE-LOG-BUCKET-NAME>.id
 
   rule {
     apply_server_side_encryption_by_default {
