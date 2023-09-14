1a7e1558e1cf0f66889665369410544a6a0614fc
kwehen <kwesihenry11@gmail.com>
2023-09-14 07:35:16 -0400

    modified dev-env.tf
    formatted the file

--- ansible/dev-env.tf
+++ ansible/dev-env.tf
@@ -1,21 +1,21 @@
 resource "aws_vpc" "dev-vpc" {
-  cidr_block = "10.1.0.0/16"
+  cidr_block           = "10.1.0.0/16"
   enable_dns_hostnames = true
-  
+
   tags = {
     Name = "dev-vpc"
   }
 }
 
 resource "aws_subnet" "dev-subnet" {
-  vpc_id = aws_vpc.dev-vpc.id
-  cidr_block = "10.1.1.0/24"
+  vpc_id                  = aws_vpc.dev-vpc.id
+  cidr_block              = "10.1.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone       = "us-east-1a"
 
-    tags = {
-        Name = "dev-subnet"
-    }
+  tags = {
+    Name = "dev-subnet"
+  }
 }
 
 resource "aws_internet_gateway" "dev-igw" {
@@ -35,35 +35,35 @@ resource "aws_route_table" "dev-route-table" {
 }
 
 resource "aws_route" "dev-route" {
-  route_table_id = aws_route_table.dev-route-table.id
+  route_table_id         = aws_route_table.dev-route-table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.dev-igw.id
+  gateway_id             = aws_internet_gateway.dev-igw.id
 }
 
 resource "aws_route_table_association" "dev-route-assoc" {
-  subnet_id = aws_subnet.dev-subnet.id
+  subnet_id      = aws_subnet.dev-subnet.id
   route_table_id = aws_route_table.dev-route-table.id
 }
 
 resource "aws_security_group" "dev-SG" {
-  name = "dev-SG"
+  name        = "dev-SG"
   description = "Allow all traffic from my IP and Internally"
-  vpc_id = aws_vpc.dev-vpc.id
+  vpc_id      = aws_vpc.dev-vpc.id
 
   ingress {
     description = "Allow all traffic from my IP"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = [
-        "198.44.128.149/32",
-        "10.1.1.0/32"]
+      "198.44.128.149/32",
+    "10.1.1.0/32"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -73,21 +73,21 @@ resource "aws_security_group" "dev-SG" {
 }
 
 resource "aws_key_pair" "tft-auth" {
-  key_name = "tft-key"
+  key_name   = "tft-key"
   public_key = file("~/.ssh/tft-key.pub")
 }
 
 resource "aws_instance" "dev-ubuntu" {
-  ami = data.aws_ami.server_ami.id
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.tft-auth.id
+  ami                    = data.aws_ami.server_ami.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.tft-auth.id
   vpc_security_group_ids = [aws_security_group.dev-SG.id]
-  subnet_id = aws_subnet.dev-subnet.id
-  user_data = file("userdata.tpl")
+  subnet_id              = aws_subnet.dev-subnet.id
+  user_data              = file("userdata.tpl")
 
   root_block_device {
     volume_size = 10
-    encrypted = true
+    encrypted   = true
   }
 
   tags = {
@@ -96,15 +96,15 @@ resource "aws_instance" "dev-ubuntu" {
 }
 
 resource "aws_instance" "dev-windows" {
-  ami = data.aws_ami.windows-server-ami.id
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.tft-auth.id
+  ami                    = data.aws_ami.windows-server-ami.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.tft-auth.id
   vpc_security_group_ids = [aws_security_group.dev-SG.id]
-  subnet_id = aws_subnet.dev-subnet.id
+  subnet_id              = aws_subnet.dev-subnet.id
 
   root_block_device {
     volume_size = 30
-    encrypted = true
+    encrypted   = true
   }
 
   tags = {
